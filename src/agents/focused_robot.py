f470a13e342185f7ebe0dc6313c77ef34f5d7373
lagcleaner <l.garcia3@estudiantes.matcom.uh.cu>
2020-12-07 13:25:16 -0500

    [agents] focused robot implemented

--- src/agents/focused_robot.py
+++ src/agents/focused_robot.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Any
 from random import randint
+from queue import Queue
 
 from .agent import HouseAgent
 from ..commons.directions import Directions
@@ -13,7 +14,7 @@ from .utils import Infinite
 class FocusedRobot(HouseAgent):
     '''
         As the name describes focus the attention and dont stop until finish,
-        the tasks, once is choicing a target the preference is setted:
+        the tasks, once is choicing a target, the preference is setted like:
 
         `focus_child = True` then until the last kid out of the playpen does
         not set the next goal
@@ -26,31 +27,66 @@ class FocusedRobot(HouseAgent):
         super().__init__(coord)
         self.focus_child = focus_child or not focus_garbage
         self.goal: Coordinates = None
+        self.directions_path: List[Tuple[int, int]] = None
 
-    def action(self, env_info) -> AgentAction:
-        if self.goal is None and self.set_next_goal(env_info) is None:
+    def action(self, env_info) -> [AgentAction]:
+        if self.set_goal(env_info) is None:
             # if there is no goal available
-            return AgentAction.Stay
+            return [AgentAction.Stay]
         if self.coord == self.goal:
-            return self.action_on_cell(env_info)
-        move = self.next_move(env_info)
+            # if t is in the goal cell
+            return [self.action_on_cell(env_info)]
+        # move to the goal
+        moves = self.next_move(env_info, 1+self.double_steping_allowed)
+        return moves if moves else [AgentAction.Stay]
 
-    def next_move(self, env_info):
-        visited_on_dfs_s = set()
-        for dr in Directions.ALL:
-            adyc = Coordinates.on_direction(self.coord, dr)
-            if env_info.in_range(adyc):
-                continue
-            if self.dfs(
-                env_info, adyc, visited_on_dfs_s,
-                lambda env, coord: env[coord] == self.goal
-            ):
-                if self.carrying:
-                    # double stepping if possible
-                    sec_degre_ady = Coordinates.on_direction(adyc, dr)
-                    if env_info.in_range(sec_degre_ady) and env_info[sec_degre_ady] in (CellContent.Empty, CellContent.Dirty):
+    def next_move(self, env_info, steps=1):
+        if not self.directions_path:
+            return [AgentAction.Stay]
+        actions = []
+        cur_coord = self.coord
+        # while all steps where defined and
+        # still having positions movements to get to the goal
+        while steps > 0 and self.directions_path:
+            dr = self.directions_path.pop()
+            cur_coord = Coordinates.on_direction(cur_coord, dr)
+            cur_action = AgentAction(Directions.ALL.index(dr) + 1)
+            if self.goal == cur_coord:
+                return [cur_action]
+            actions.append(cur_action)
+            steps -= 1
 
-                if self.coord
+        return actions
+
+    def bfs_path(self, env_info, coord: Coordinates, condition) -> List[Coordinates]:
+        if condition(env_info, coord):
+            return coord
+        queue = Queue()
+        #
+        queue.put(coord)
+        path_d = {coord: (None, None)}
+        #
+        while not queue.empty():
+            c = queue.get()
+            #
+            for dr in Directions.ALL:
+                ca = Coordinates.on_direction(c, dr)
+                if ca in path_d or not env_info.in_range(ca):
+                    continue
+                if condition(env_info, ca):
+                    path = [dr]
+                    step_c = c
+                    while step_c == None:
+                        step_c, dirct = path_d.get(step_c, (None, None))
+                        if step_c == None:
+                            break
+                        path.append(dirct)
+
+                        # build path of directions
+                    return path
+                path_d[ca] = c, dr
+                queue.put(ca)
+        return None
 
     def action_on_cell(self, env_info):
         if env_info[self.coord] == CellContent.Dirty:
@@ -62,8 +98,12 @@ class FocusedRobot(HouseAgent):
             if child.coord == self.coord
         ):
             return AgentAction.CarryAChild
+        return AgentAction.Stay
 
-    def set_next_goal(self, env_info):
+    def set_goal(self, env_info):
+        self.goal = None
+        self.directions_path = None
+        #
         if self.focus_child:
             self.goal = self.set_next_child(env_info)
             if self.goal is None:
@@ -72,6 +112,13 @@ class FocusedRobot(HouseAgent):
             self.goal = self.set_next_dirt(env_info)
             if self.goal is None:
                 self.goal = self.set_next_child(env_info)
+
+        # build path if there is a goal
+        if self.goal:
+            self.directions_path = self.bfs_path(
+                env_info, self.coord,
+                lambda env, coord: coord == self.goal
+            )
         return self.goal
 
     def set_next_child(self, env_info):
@@ -85,10 +132,6 @@ class FocusedRobot(HouseAgent):
                     if best_dist > dist
                     else (nearest_child, best_dist)
                 )
-        self.next_move = self.bfs(
-            env_info, self.coord,
-            lambda env, coord: coord == nearest_child.coord
-        )
         return nearest_child
 
     def set_next_dirt(self, env_info):
