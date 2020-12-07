d7b43bc512cc3d34ba14c9fc5190cfaef18ff0f5
lagcleaner <l.garcia3@estudiantes.matcom.uh.cu>
2020-12-07 00:45:27 -0500

    [agents] change the way to get adyacent in the agents

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -20,29 +20,5 @@ class HouseAgent:
     def doble_steping_allowed(self) -> bool:
         raise NotImplementedError
 
-    def adyacents(self, house):
-        pass
-        # c, r = self.coord
-        # adyacent_pos = [
-        #     (dc + c, dr + r)
-        #     for dc, dr in DIRECTIONS
-        #     if 0 <= dc + c < house.dim.cols and 0 <= dr + r < house.dim.rows
-        # ]
-        # if self.doble_steping:
-        #     adyacent_pos.extend([
-        #         (2*dc + c, 2*dr + r)
-        #         for dc, dr in DIRECTIONS
-        #         if 0 <= 2*dc + c < house.dim.cols and 0 <= 2*dr + r < house.dim.rows
-        #     ])
-        # return adyacent_pos
-
-    def move_to(self, house, direction=(1, 0)):
-        pass
-        # c, r = self.coord
-        # nc, nr = c + direction[0], r + direction[1]
-        # if house.field[nc][nr].can_hold(self):
-        #     house.field[nc][nr].hold(self)
-        #     house.field[c][r].release(self)
-        #     return True
-        # else:
-        #     return False
+    def adyacents(self, env, distance=1) -> Sequence[Coordinates]:
+        return env.available_directions(self.coord, distance == 2)
