d03decac2dca0a941d78784064c8144e5bca47af
lagcleaner <l.garcia3@estudiantes.matcom.uh.cu>
2020-12-07 00:38:46 -0500

    [commons] create a move method in coordinates for returning another coordinates obj

--- src/commons/coordinates.py
+++ src/commons/coordinates.py
@@ -1,6 +1,15 @@
 from typing import NamedTuple
+from .directions import Directions
+
+
+def build_coordinates(coord_t: tuple):
+    return Coordinates(col=coord_t[0], row=coord_t[1])
 
 
 class Coordinates(NamedTuple):
     col: int
     row: int
+
+    @staticmethod
+    def on_direction(coord, dr: Directions):
+        return build_coordinates((coord[0] + dr[0], coord[1] + dr[1]))
